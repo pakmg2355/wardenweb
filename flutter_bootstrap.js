b2e22e9feaef1dc96bd7b1cb864dec16077be9ab
Pak MG MG <pakmg2355@hotmail.com>
2026-01-17 13:36:47 +0100

    build web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "633788186"
+    serviceWorkerVersion: "3289552635"
   }
 });
